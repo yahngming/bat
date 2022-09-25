98965002489a8a684b28051ebf3bc3a0abdc7de3
Yang Ming <yahngming@gmail.com>
2022-09-25 21:32:11 +0800

    Add files via upload

---
+++ coffee.bat
@@ -0,0 +1,6 @@
+@echo off
+for /f "delims=" %%a in ('powercfg -getactivescheme') do (
+  echo %%a | find /i "balanced" > nul && powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+  echo %%a | find /i "performance" > nul && powercfg -s 381b4222-f694-41f0-9685-ff5bb260df2e
+)
+powercfg -getactivescheme
